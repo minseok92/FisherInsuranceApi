98c407686e0dfdc330196fabfc81aca05b3be486
minseok92 <kim.3926@osu.edu>
2017-02-15 18:13:33 -0500

    Update autocontroller to use memorystore

--- Controllers/AutoController.cs
+++ Controllers/AutoController.cs
@@ -1,13 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
+using FisherInsuranceApi.Data;
+using FisherInsuranceApi.Models;
 
 [Route("api/auto/quotes")]
 public class AutoController : Controller
 {
+    private IMemoryStore db;
+    public AutoController(IMemoryStore repo)
+    {
+        db = repo;
+    }
+
+    [HttpGet]
+    public IActionResult GetQuotes()
+    {
+        return Ok(db.RetrieveAllQuotes);
+    }
+
     //POST api/auto/quotes
     [HttpPost]
-    public IActionResult Post([FromBody]string value)
+    public IActionResult Post(int id, [FromBody] Quote quote)
     {
-        return Created("",value);
+        return Ok(db.CreateQuote(quote));
     }
 
     //GET api/auto/quotes/5
@@ -15,23 +29,24 @@ public class AutoController : Controller
     [HttpGet("{id}")]
     public IActionResult Get(int id)
     {
-        return Ok("The id is: " + id);
+        return Ok(db.RetrieveQuote(id));
     }
 
     //PUT api/auto/quotes/id
 
     [HttpPut("{id}")]
-    public IActionResult Put(int id, [FromBody]string value)
+    public IActionResult Put(int id, [FromBody] Quote quote)
     {
-        return NoContent();
+        return Ok(db.UpdateQuote(quote));
     }
 
     //DELETE api/auto/quotes/id
 
     [HttpDelete("{id}")]
-    public IActionResult Delete(int id)
+    public IActionResult Delete(int id, [FromBody] Quote quote)
     {
-        return Delete(id);
+        db.DeleteQuote(id);
+        return Ok();
     }
 
 }
