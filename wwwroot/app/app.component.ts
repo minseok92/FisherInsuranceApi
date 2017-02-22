89b512656438b59e09e59e4b2d2c84c440674d47
minseok92 <kim.3926@osu.edu>
2017-02-22 18:19:24 -0500

    Adding the Home Page Component

--- wwwroot/app/app.component.ts
+++ wwwroot/app/app.component.ts
@@ -2,7 +2,7 @@ import {Component} from "@angular/core";
 
 @Component({
     selector: "fisher-insurance",
-    template: '<h1>Hello Fisher Insurance<h1>'
+    template: '<home-page>'
 })
 
 export class AppComponent {}
